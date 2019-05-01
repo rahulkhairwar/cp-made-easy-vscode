5c8320db53dc7ab392e2a4651a36390640ca1a6c
Rahul Khairwar <rahulkhairwar@gmail.com>
2019-05-02 04:38:44 +0530

    Completed ContestsListProvider, and added ContestProblemsProvider

--- src/utils/CodeforcesAPIUtils.ts
+++ src/utils/CodeforcesAPIUtils.ts
@@ -1,34 +1,30 @@
 import { Contest } from "../dto/Contest";
-import { Properties } from "ts-json-properties";
 import * as request from 'request';
+
+let Properties = require('../../properties.json');
 
 /**
  * A utility class to fetch data from Codeforces through it's defined API.
  * Read about it's API <a href="https://codeforces.com/api/help">here<a/>
  */
 export class CodeforcesAPIUtils {
-    constructor() {
-        let propertiesFile = "./properties.json";
-        Properties.initialize(propertiesFile);
-        // console.log("prop.cf : " + Properties.get("CODEFORCES_CONTEST_LIST_URL"));
-        // console.log("Prop.toString() : " + Properties.toString());
-    }
+/*     constructor() {
+    } */
 
     public getContestsList(callback: (contests: Contest[]) => any) {
-        console.log("entered getContestsList()");
-
-        // NOT WORKING!!! DON'T KNOW WHY!! >.<
-        let cfContestListUrl = Properties.get("CODEFORCES_CONTEST_LIST_URL");
-        console.log(`cfContestListUrl : ${cfContestListUrl}`);
-        let CODEFORCES_CONTEST_LIST_URL = "https://www.codeforces.com/api/contest.list";
-
-        request.get(CODEFORCES_CONTEST_LIST_URL, (error: any, response: any, body: any) => {
+        let cfContestListUrl = Properties["CODEFORCES_CONTEST_LIST_URL"];
+        // let CODEFORCES_CONTEST_LIST_URL = "https://www.codeforces.com/api/contest.list";
+        
+        request.get(cfContestListUrl, (error: any, response: any, body: any) => {
             let jsonObject = JSON.parse(body);
             let contestsArray = jsonObject.result.map((contest: any) => new Contest(contest));
 
-            console.log("returning from getContestsList()");
             callback(contestsArray);
         });
+    }
+
+    public getContestDetails() {
+        //
     }
 
 }
@@ -37,11 +33,11 @@ export class CodeforcesAPIUtils {
 /* let cfApiUtils = new CodeforcesAPIUtils();
 
 cfApiUtils.getContestsList((contests: Contest[]) => {
-    var len = contests.length;
+    let len = contests.length;
 
     console.log("No. of contests : " + len);
 
-    for (var i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
         console.log(`i : ${i}, contests[i] : ${contests[i]}`);
     }
 }); */
